33f0f62c68d5d3c6f582486e8fba412bf9df06df
Harry Lee <leelee@fullsail.edu>
2012-09-09 13:42:22 -0400

    Up date site1.js with CSV code. Add static data to the list1.php.

--- Gold App/js/site1.js
+++ Gold App/js/site1.js
@@ -1,11 +1,19 @@
 $(function(){
 	
 	$.ajax({
-		//url: 'xhr/list.php',
+		url: 'xhr/list1.php',
 		type: 'GET',
-		dataType: 'CSV ',
+		dataType: 'CSV',
 		success: function(response){
-			console.log(response);
-		}
-	});
+			var lines = data.split("\n");
+			for (var lineNum = 0; lineNum < lines.length; lineNum++) {
+			var row = line[lineNum];
+			var columns = row.split(",");
+			console.log(columns);
+		).appendTo('#CSV');
+		
+			}
+			)
+		};
+	};
 });
---
+++ Gold App/xhr/list1.php
@@ -0,0 +1,12 @@
+	    "lists", "dateRead","author""title","publicationDate","bookSeries",	"genre","age","subject","descriptionOfBook","rate","comments",			
+	    "Lists: Books Read", "Date Read: 4/24/2012","Author: Brandon Sanderson","Title: Warbreaker","Publication Date: 3/30/2010","Book Series:","Genre: Fiction","Age: Adult","Subject: Science Fiction & Fantasy","Description of Book:","Rate: 8", 69788
+		 				
+							
+			 	
+					
+		 				
+		 					
+		 				
+			
+							
+					 	
