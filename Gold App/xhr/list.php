b6e96d23fc9e3460b9633d65f95565c84549860d
Harry Lee <leelee@fullsail.edu>
2012-09-09 13:32:00 -0400

    Upgrade my application to use json. Add data into list.php.  Add code into site.js.

--- Gold App/xhr/list.php
+++ Gold App/xhr/list.php
@@ -1,37 +1,50 @@
 {
-	"book1": 
+	"book1": [ 
 		{
-	    "lists:" ["Lists:", "Books Read"],
-		"author": ["Author:", "Brandon Sanderson"],
-		"title": ["Title:", "Warbreaker" ],
-		"date":	 ["Date:", "2012-04-20"],
-		"genre": ["Genre", "Fiction"],
-		"age": ["Age:", "Adult"],
-		"subject": ["Subject:", "Science Fiction & Fantasy"],
-		"rate":	["Rate:", "8"],
-		"comments":	["Comments", "69788"]
-	},
-	"book2": {
-	    "lists:": 		["Lists:", "Books Read"],
-		"author": 		["Author:", "Jonathan Stroud"],
-		"title":  		["Title:", "The Ring of Solomon"],
-		"date":	  		["Date:", "2012-04-21"],
-		"genre":		["Genre", "Fiction"],
-		"age":			["Age:", "Adult"],
-		"subject":		["Subject:", "Science Fiction & Fantasy"],
-		"rate":			["Rate", "10"],
-		"comments":		["Comments", "72274"]
-	},
-	"book3": {
-	    "lists:": 		["Lists:", "Book I will read"],
-		"author": 		["Author:", "Aliso Goodman"],
-		"title":  		["Title:", "Eona"],
-		"date":	  		["Date:",],
-		"genre":		["Genre", "Nonfiction"],
-		"age":			["Age:", "Adult"],
-		"subject":		["Subject:", "Science Fiction & Fantasy"],
-		"rate":			["Rate", "1"],
-		"comments":		["Comments", "73260"]
+	    "lists": 				"Lists: Books Read",
+	    "dateRead":				"Date Read: 4/24/2012",
+		"author": 				"Author: Brandon Sanderson",
+		"title": 				"Title: Warbreaker",
+		"publicationDate":	 	"Publication Date: 3/30/2010",
+		"bookSeries":			"Book Series:",
+		"genre": 				"Genre: Fiction",
+		"age": 					"Age: Adult",
+		"subject": 				"Subject: Science Fiction & Fantasy",
+		"descriptionOfBook":	"Description of Book:",
+		"rate":					"Rate: 8",
+		"comments":			 	 69788
 		
-	}
+		},
+		{
+		"lists": 				"Lists: Books Read",
+		"dateRead":				"Date Read: 6/10/2012",
+		"author": 				"Author: Jonathan Stroud",
+		"title":  				"Title: The Ring of Solomon",
+		"publicationDate":		"Publication Date: 1/24/2012",
+		"bookSeries":			"Book Series:",
+		"genre":				"Genre: Fiction",
+		"age":					"Age: Adult",
+		"subject":				"Subject: Science Fiction & Fantasy",
+		"descriptionOfBook":	"Description of Book:",
+		"rate":					"Rate: 10",
+		"comments":				72274
+		
+		},
+		{
+		"lists": 				"Lists: Book I will read",
+		"dateRead":				"Date Read:",
+		"author": 				"Author: Aliso Goodman",
+		"title":  				"Title: Eona",
+		"publicationDate":		"Publication Date: 4/19/2011",
+		"bookSeries":			"Book Series:",
+		"genre":				"Genre: Fiction",
+		"age":					"Age: Adult",
+		"subject":				"Subject: Science Fiction & Fantasy",
+		"descriptionOfBook":	"Description of Book:",
+		"rate":					"Rate: 1",
+		"comments":				73260
+		
+			}
+		]
 }
+
